4656f99a0780fdeca7df86b76732267696d3019d
mikekulka <45544526+mikekulka@users.noreply.github.com>
2022-12-31 17:51:22 -0500

    Adding in strings file and story board refrence

---
+++ LifesJourney/Controllers/SignInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignInViewController.swift
+//  LifesJourney
+//
+//  Created by Michael Kulka on 12/31/22.
+//
+
+import Foundation
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ LifesJourney/Strings.swift
@@ -0,0 +1,13 @@
+//
+//  Strings.swift
+//  LifesJourney
+//
+//  Created by Michael Kulka on 12/31/22.
+//
+
+import Foundation
+
+struct Strings {
+    let signInSegue = "SignInSegue"
+    
+}
